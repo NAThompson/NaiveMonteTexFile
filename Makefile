753719ccdbaf818124ad5692780908a55f84e435
Nick Thompson <nathompson7@protonmail.com>
2018-02-23 14:16:34 -0600

    Destruction by not employing Kahan.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXX := g++-7
 CPPFLAGS := --std=c++11 -O3 -march=native
-KILLKAHAN := -funsafe-math-optimizations
+KILLKAHAN := -funsafe-math-optimizations -ffast-math
 INCFLAGS := -I../boost/libs/math/include -I../boost/libs/math/atomic/include -I../boost/libs/lockfree/include -I../boost
 
 all: final bailey_example.x
@@ -12,7 +12,7 @@ final:
 	pdflatex -shell-escape NaiveMonteCarloOnModernHardware
 
 bailey_example.x: bailey_example.cpp
-	$(CXX) $(CPPFLAGS) $(INCFLAGS) $< -o $@ -pthread
+	$(CXX) $(CPPFLAGS) $(KILLKAHAN) $(INCFLAGS) $< -o $@ -pthread
 
 clean:
 	rm -f NaiveMonteCarloOnModernHardware.bbl  NaiveMonteCarloOnModernHardware.log  NaiveMonteCarloOnModernHardware.pdf NaiveMonteCarloOnModernHardware.aux  NaiveMonteCarloOnModernHardware.blg  NaiveMonteCarloOnModernHardware.out *.x
--- bailey_example.cpp
+++ bailey_example.cpp
@@ -24,7 +24,7 @@ void display_progress(double progress,
               << ", time to completion: "
               << estimated_time_to_completion.count()
               << " seconds, estimate: "
-              << std::setprecision(5)
+              << std::setprecision(6)
               << current_estimate
               << "     \r";
 
@@ -64,20 +64,23 @@ void calcE2()
 }
 
 template<class Real>
-void calc_pi()
+void calc_pi(Real error_goal)
 {
-    auto f = [](std::vector<Real> const & x)->Real
+    auto g = [](std::vector<Real> const & x)->Real
     {
-        Real r = x[0]*x[0] + x[1]*x[1];
-        if (r <= 1) {
-          return 4;
+        Real r = x[0]*x[0]+x[1]*x[1];
+        if (r <= 1)
+        {
+            return 4;
         }
         return 0;
     };
-    std::vector<std::pair<Real, Real>> bounds{{0.0, 1.0}, {0.0, 1.0}};
-    Real error_goal = 0.00001;
-    naive_monte_carlo<Real, decltype(f)> mc(f, bounds, error_goal, false, 2);
+
+    std::vector<std::pair<Real, Real>> bounds{{Real(0), Real(1)}, {Real(0), Real(1)}};
+    naive_monte_carlo<Real, decltype(g)> mc(g, bounds, error_goal,
+                                            /*singular =*/ false);
     auto task = mc.integrate();
+
     while (task.wait_for(std::chrono::seconds(1)) != std::future_status::ready)
     {
         display_progress(mc.progress(),
@@ -88,50 +91,23 @@ void calc_pi()
     std::cout << std::endl;
 
     Real E2 = task.get();
-    std::cout << std::setprecision(std::numeric_limits<Real>::digits10+2);
+    std::cout << std::setprecision(std::numeric_limits<Real>::digits10+2) << std::fixed;
     std::cout << "E_2 (estimated) = " << E2 << std::endl;
     Real exact = 3.1415926535897932384626433;
     std::cout << "E_2 (exact)     = " << exact << std::endl;
     std::cout << "Error      = " << E2 - exact << std::endl;
     std::cout << "Error goal = " << error_goal << std::endl;
+    std::cout << "Calls: " << mc.calls() << std::endl;
+    std::cout << "Final variance estimate: " << mc.variance() << std::endl;
 }
 
 
-template<class Real>
-void calc_close_to_avg()
-{
-    auto f = [](std::vector<Real> const & x)->Real
-    {
-        Real r = 100*std::numeric_limits<Real>::epsilon()*std::sin(2*M_PI*10*x[0]);
-        return r;
-    };
-    std::vector<std::pair<Real, Real>> bounds{{0.0, 1.0}};
-    Real error_goal = 0.0000000001;
-    naive_monte_carlo<Real, decltype(f)> mc(f, bounds, error_goal, false);
-    auto task = mc.integrate();
-    while (task.wait_for(std::chrono::seconds(1)) != std::future_status::ready)
-    {
-        display_progress(mc.progress(),
-                         mc.current_error_estimate(),
-                         mc.current_estimate(),
-                         mc.estimated_time_to_completion());
-    }
-    std::cout << std::endl;
 
-    Real E2 = task.get();
-    std::cout << std::setprecision(std::numeric_limits<Real>::digits10+2);
-    std::cout << "E_2 (estimated) = " << E2 << std::endl;
-    Real exact = 1.0;
-    std::cout << "E_2 (exact)     = " << exact << std::endl;
-    std::cout << "Error      = " << E2 - exact << std::endl;
-    std::cout << "Error goal = " << error_goal << std::endl;
-    std::cout << "Calls" << mc.calls() << std::endl;
-}
 
 int main()
 {
   //calcE2<double>();
   //calcE2<float>();
-  //calc_pi<float>();
-  calc_close_to_avg<float>();
+  //calc_pi<float>(0.0001);
+  calc_pi<double>(0.000001);
 }
